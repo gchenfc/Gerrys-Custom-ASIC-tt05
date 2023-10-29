2dbf096510a8a97466cfa5c2f8b009da9174b1a7
Gerry Chen <gerry.chen2015@gmail.com>
2023-10-29 18:24:49 -0400

    switch rollover to wire instead of registered

--- src/rollover.v
+++ src/rollover.v
@@ -3,18 +3,14 @@ module rollover (
     output wire [4:0] limit
 );
 
-    always @(*) begin
-        case(name)         // 012345678901234567
-            0: limit = 11; // Gerry Chen .
-            1: limit = 17; // Fonda Chen Kelly .
-            2: limit = 10; // Jim Kelly .
-            3: limit = 11; // Aven Kelly .
-                           // 012345678901234567
-            4: limit = 13; // Virginia Pan .
-            5: limit = 11; // Hanna Chen .
-            6: limit = 11; // Xinfu Chen .
-            7: limit = 11; // Joyce Chen .
-        endcase
-    end
+    assign limit = (name == 3'b000) ? 11 :  // Gerry Chen
+                   (name == 3'b001) ? 17 :  // Fonda Chen-Kelly
+                   (name == 3'b010) ? 10 :  // Jim Kelly
+                   (name == 3'b011) ? 11 :  // Aven Kelly
+                   (name == 3'b100) ? 13 :  // Virginia Pan
+                   (name == 3'b101) ? 11 :  // Hanna Chen
+                   (name == 3'b110) ? 11 :  // Xinfu Chen
+                   (name == 3'b111) ? 11 :  // Joyce Chen
+                    5'b11111;  // default
 
 endmodule
